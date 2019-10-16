ca1ff0638f382db6e9c1cc811bb133906b84398b
jieD <38785315+huangketsudou@users.noreply.github.com>
2019-10-16 15:37:20 +0800

    Update palindrome.py

--- string/palindrome.py
+++ string/palindrome.py
@@ -7,5 +7,17 @@ class Solution:
             return True
         return False
 
+class Solution2:
+    def isPalindrome(self,x):
+        if x<0 or (x%10==0 and x!=0):
+            return False
+        y=0
+        while y<x:
+            pop=x%10
+            y=y*10+pop
+            x//=10
+        return x==y or x//10==y
+    
+    
 k=Solution()
 k.isPalindrome(123)
