fa4639471c1f524ab12443c0456e45db4f817c9d
jieD <38785315+huangketsudou@users.noreply.github.com>
2019-10-16 14:42:09 +0800

    Create palindrome.py

---
+++ string/palindrome.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
+        strx=str(x)
+        if strx==strx[::-1]:
+            return True
+        return False
+
+k=Solution()
+k.isPalindrome(123)
