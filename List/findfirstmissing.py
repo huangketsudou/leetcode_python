292d518a47c59a6a0c99b4dbbde14f280b945337
jieD <38785315+huangketsudou@users.noreply.github.com>
2019-10-29 10:49:31 +0800

    Create findfirstmissing.py

---
+++ List/findfirstmissing.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n=len(nums)
+        i=0
+        while i<n:
+            while nums[i]!=i+1 and 0<nums[i]<=n:
+                #确保先修改的是nums[nums[i]-1],而不会先对nums[i]造成修改
+                nums[nums[i]-1],nums[i]=nums[i],nums[nums[i]-1]
+                #nums[i],nums[nums[i]-1]=nums[nums[i]-1],nums[i]
+                #这句话是错的
+            i+=1
+        for i,num in enumerate(nums):
+            if num!=i+1:
+                return i+1
+        return n+1
+
+
+k=Solution()
+b=[3,4,-1,1]
+print(k.firstMissingPositive(b))
+print(b)
