7128cb668e30876f961334ede0f257fdaae0df4a
jieD <38785315+huangketsudou@users.noreply.github.com>
2019-11-04 22:05:06 +0800

    Create permuteunique.py

---
+++ List/permuteunique.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        answer=[]
+        n=len(nums)
+        nums.sort()
+        tmp=[]
+        used=[]
+        self.core(n,answer,nums,tmp,used)
+        return answer
+
+
+    def core(self,n,answer,nums,tmp,used):
+        if len(used)==n:
+            answer.append(tmp[:])
+        for i in range(n):
+            if i in used:
+                continue
+            if nums[i]==nums[i-1] and i-1 not in used:
+                continue
+            tmp.append(nums[i])
+            used.append(i)
+            self.core(n,answer,nums,tmp,used)
+            used.pop(-1)
+            tmp.pop(-1)
+
+
+
+k=Solution()
+print(k.permuteUnique([3,1,1]))
