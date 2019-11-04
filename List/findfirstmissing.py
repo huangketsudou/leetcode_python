6eedce105fa733145274cb73c1b44f2c50ead8e6
jieD <38785315+huangketsudou@users.noreply.github.com>
2019-11-04 10:41:37 +0800

    Update findfirstmissing.py

--- List/findfirstmissing.py
+++ List/findfirstmissing.py
@@ -6,7 +6,9 @@ class Solution:
         n=len(nums)
         i=0
         while i<n:
-            while nums[i]!=i+1 and 0<nums[i]<=n:
+            while nums[i]!=i+1 and 0<nums[i]<=n and nums[i]!=nums[nums[i]-1]:
+                # if nums[i]<=0 or nums[i]>n or nums[i]==nums[nums[i]-1]:
+                #     break
                 #确保先修改的是nums[nums[i]-1],而不会先对nums[i]造成修改
                 nums[nums[i]-1],nums[i]=nums[i],nums[nums[i]-1]
                 #nums[i],nums[nums[i]-1]=nums[nums[i]-1],nums[i]
@@ -17,8 +19,8 @@ class Solution:
                 return i+1
         return n+1
 
-
 k=Solution()
-b=[3,4,-1,1]
-print(k.firstMissingPositive(b))
-print(b)
+print(k.firstMissingPositive([1,3,3]))
+
+k.firstMissingPositive([1,1]）
+k.firstMissingPositive([3,4,-1,1])
