5f359ce7f6e1ad0e120b18bb8980e17af71680a1
jieD <38785315+huangketsudou@users.noreply.github.com>
2019-11-07 10:23:59 +0800

    Create canjump.py

---
+++ List/canjump.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        start = 0
+        end = 0
+        n = len(nums)
+        while start <= end and end < len(nums) - 1:
+            end = max(end, nums[start] + start)
+            start += 1
+        return end >= n - 1
+
+
+class Solution2:
+    def canJump(self, nums: List[int]) -> bool:
+        #start = 0
+        n = len(nums)
+        start = n - 2
+        end = n - 1
+        while start >= 0:
+            if start + nums[start] >= end: end = start
+            start -= 1
+        return end <= 0
+
+
+k=Solution()
+print(k.canJump([3,0,8,2,0,0,1]))
