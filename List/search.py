95b13610b2014ea7b0060bd9b0c723c22a9846d7
jieD <38785315+huangketsudou@users.noreply.github.com>
2019-11-14 11:13:14 +0800

    Update search.py

--- List/search.py
+++ List/search.py
@@ -3,6 +3,7 @@ from typing import List
 
 
 class Solution:
+    #元素不重复
     def search(self, nums: List[int], target: int) -> int:
         # 判断target值是在[左边界, 中值]区间还是[右边界, 中值]区间
         left, right = 0, len(nums) - 1
@@ -26,3 +27,33 @@ class Solution:
 
 k=Solution()
 print(k.search([5,5,6,1,2,3],4))
+
+
+
+class Solution:
+    #元素可重复
+    def search(self, nums: List[int], target: int) -> bool:
+        n=len(nums)
+        if n==0:return False
+        left,right=0,n-1
+        while left<=right:
+            mid=(left+right)//2
+            if nums[mid]==target:return True
+            if nums[mid]==nums[left]==nums[right]:
+                right-=1
+                left+=1
+            elif nums[left]<=nums[mid]:
+                if nums[left]<=target<nums[mid]:
+                    right=mid-1
+                else:
+                    left=mid+1
+            else:
+                if nums[mid]<target<=nums[right]:
+                    left=mid+1
+                else:
+                    right=mid-1
+        return False
+
+
+k=Solution()
+print(k.search([2,5,6,0,0,1,2],2))
