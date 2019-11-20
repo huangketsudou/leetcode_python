99bb21bca6b44ba28b70763db051e75b2f53f6f4
jieD <38785315+huangketsudou@users.noreply.github.com>
2019-11-20 10:04:31 +0800

    Update subsets.py

--- List/subsets.py
+++ List/subsets.py
@@ -31,6 +31,44 @@ class Solution:
                 answer.append(list(j))
         return answer
 
+    
+class Solution2:
+    #有重复数据的版本
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+
+
+        def combinations(iterable, r):
+            # combinations('ABCD', 2) --> AB AC AD BC BD CD
+            # combinations(range(4), 3) --> 012 013 023 123
+            pool = tuple(iterable)
+            n = len(pool)
+            if r > n:
+                return
+            indices = list(range(r))
+            yield tuple(pool[i] for i in indices)  # 对应solution3最开始加入的一个情况
+            # 这个算法主要比较像把所要求的指标逐个后移
+            while True:
+                for i in reversed(range(r)):
+                    if indices[i] != i + n - r:
+                        break
+                else:
+                    return
+                indices[i] += 1
+                for j in range(i + 1, r):
+                    indices[j] = indices[j - 1] + 1
+                yield tuple(pool[i] for i in indices)
+
+
+        nums.sort()
+        n = len(nums)
+        answer = []
+        for i in range(n + 1):
+            for j in combinations(nums, i):
+                lj=list(j)
+                if lj not in answer:
+                    answer.append(lj)
+        return answer    
+
 
 k=Solution()
 print(k.subsets([1,2,3]))
