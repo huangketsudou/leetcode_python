080013f3f6f62008769d0942f0fccb246a276364
jieD <38785315+huangketsudou@users.noreply.github.com>
2019-11-23 22:15:44 +0800

    Update buildTree.py

--- List Node/buildTree.py
+++ List Node/buildTree.py
@@ -25,6 +25,7 @@ class Solution2:
 
 
 class Solution:
+    #这个用前序和中序合成
     #注意list.index是线性查找，最坏的情况退化到O(n**2),可以利用哈希表实现最坏O(n)
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
         if not preorder:
@@ -36,8 +37,21 @@ class Solution:
         root.right=self.buildTree(preorder[1+cur:],inorder[cur+1:])
         return root
 
+    
+class Solution3:
+    #注意list.index是线性查找，最坏的情况退化到O(n**2),可以利用哈希表实现最坏O(n)
+    #这个用中序和后序
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        if not postorder:
+            return None
+        index=postorder[-1]
+        cur=inorder.index(index)
+        root=TreeNode(index)
+        root.left=self.buildTree(inorder[:cur],postorder[:cur])
+        root.right=self.buildTree(inorder[cur+1:],postorder[cur:-1])
+        return root
 
-
+#前序
 k=Solution()
 t=k.buildTree([3,9,20,15,7],[9,3,15,20,7])
 
@@ -46,5 +60,11 @@ g=Solution2()
 print(g.inorderTraversal(t))
 
 
+#后序
+k=Solution3()
+t=k.buildTree([9,3,15,20,7],[9,15,7,20,3])
 
+
+g=Solution2()
+print(g.inorderTraversal(t))
 
