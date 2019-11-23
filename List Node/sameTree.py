796c08e375aec3c2d763c2a1bee428af132fcae0
jieD <38785315+huangketsudou@users.noreply.github.com>
2019-11-23 15:37:20 +0800

    Update sameTree.py

--- List Node/sameTree.py
+++ List Node/sameTree.py
@@ -16,6 +16,39 @@ class Solution:
             return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
         return False
 
+    
+    
+    
+class Solution2:
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+
+        def check(p, q):
+            # if both are None
+            if not p and not q:
+                return True
+            # one of p and q is None
+            if not q or not p:
+                return False
+            if p.val != q.val:
+                return False
+            return True
+
+        deq = deque([(p, q), ])
+        while deq:
+            p, q = deq.popleft()
+            if not check(p, q):
+                return False
+
+            if p:
+                deq.append((p.left, q.left))
+                deq.append((p.right, q.right))
+
+        return True    
 
 qt1=TreeNode(1)
 qt2=TreeNode(2)
