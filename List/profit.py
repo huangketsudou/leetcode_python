878d34228eae8a2aebdb4c899ec97b4b163d5df7
jieD <38785315+huangketsudou@users.noreply.github.com>
2019-11-28 21:39:53 +0800

    Update profit.py

--- List/profit.py
+++ List/profit.py
@@ -33,5 +33,20 @@ class Solution2:
             maxprofit=max(maxprofit,prices[i]-minprice)
         return maxprofit
 
+    
+class Solution3:
+    #仅允许交易一次
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:return 0
+        n=len(prices)
+        dp=[[0]*2 for _ in range(n)]
+        #base case
+        for i in range(n):
+            dp[i][0]=max(dp[i-1][0],dp[i-1][1]+prices[i])
+            dp[i][1]=max(dp[i-1][1],-prices[i])
+        return dp[n-1][0]
+    
+    
+
 k=Solution()
 print(k.maxProfit([7,1,5,3,3,6,4]))
