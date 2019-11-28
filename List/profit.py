b41349178def33b2c00fb5ab1eb69dd4e3d59ab1
jieD <38785315+huangketsudou@users.noreply.github.com>
2019-11-28 22:33:24 +0800

    Update profit.py

--- List/profit.py
+++ List/profit.py
@@ -3,7 +3,7 @@
 
 
 
-class Solution:
+class Solution_inf:
     #不限制交易次数
     def maxProfit(self, prices: List[int]) -> int:
         if not prices:return 0
@@ -20,9 +20,32 @@ class Solution:
         answer+=max(0,right-left)
         return answer
 
+    
+class Solution_inf1:
+    # 不限制交易次数
+    def maxProfit(self, prices: List[int]) -> int:
+        n=len(prices)
+        maxprofit=0
+        minprice=float('-inf')
+        for i in range(n):
+            tmp=maxprofit
+            maxprofit=max(maxprofit,minprice+prices[i])
+            minprice=max(minprice,tmp-prices[i])
+        return maxprofit
 
+class Solution_inf2:
+    # 不限制交易次数
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices: return 0
+        n = len(prices)
+        dp = [[0] * 2 for _ in range(n)]
+        for i in range(1,n):
+            dp[i][0]=max(dp[i-1][0],dp[i-1][1]+prices[i])
+            dp[i][1]=max(dp[i-1][1],dp[i-1][0]-prices[i])
+        return dp[n-1][0]    
+    
 
-class Solution2:
+class Solution_1:
     #仅允许交易一次
     def maxProfit(self, prices: List[int]) -> int:
         minprice=float('inf')
@@ -34,7 +57,7 @@ class Solution2:
         return maxprofit
 
     
-class Solution3:
+class Solution_11:
     #仅允许交易一次
     def maxProfit(self, prices: List[int]) -> int:
         if not prices:return 0
