23d5ff2600834639a6c4b2178186bd3aa679af12
jieD <38785315+huangketsudou@users.noreply.github.com>
2020-01-09 20:02:30 +0800

    Update course_canfinish.py

--- graph/course_canfinish.py
+++ graph/course_canfinish.py
@@ -3,6 +3,7 @@ from typing import List
 
 
 class Solution:
+    #bfs
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         #实际上是一道探讨有向非循环图的题目
         from collections import defaultdict
@@ -26,6 +27,37 @@ class Solution:
                 if count[e]==0:
                     ready.append(e)
         return len(topo)==numCourses
+
+
+    
+    
+class Solution:
+    #dfs
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        from collections import defaultdict
+        graph = defaultdict(list)
+        # 记录
+        visited = set()
+        # 建图
+        for x, y in prerequisites:
+            graph[y].append(x)
+
+        # 深度遍历
+        def dfs(i, being_visited):
+            if i in being_visited: return False
+            if i in visited: return True
+            visited.add(i)
+            being_visited.add(i)
+            for j in graph[i]:
+                if not dfs(j, being_visited): return False
+            being_visited.remove(i)
+            return True
+        # 检测每门功课起始是否存在环
+        for i in range(numCourses):
+            # 已经访问过
+            if i in visited: continue
+            if not dfs(i, set()): return False
+        return True
 
 
 
