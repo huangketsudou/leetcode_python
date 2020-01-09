e2a46ca7e28a0044c975733a500f62cdc06da4f6
jieD <38785315+huangketsudou@users.noreply.github.com>
2020-01-09 11:06:52 +0800

    Create course_canfinish.py

---
+++ graph/course_canfinish.py
@@ -0,0 +1,34 @@
+from typing import List
+import functools
+import math
+import itertools
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        from collections import defaultdict
+        count = defaultdict(int)
+        for i in range(numCourses):
+            count[i] = 0
+        graph = defaultdict(list)
+        for i, j in prerequisites:
+            graph[j].append(i)
+            count[i] += 1
+        ready=[]
+        for k,v in count.items():
+            if v==0:
+                ready.append(k)
+        topo=[]
+        while ready:
+            u=ready.pop()
+            topo.append(u)
+            for e in graph[u]:
+                count[e]-=1
+                if count[e]==0:
+                    ready.append(e)
+        return len(topo)==numCourses
+
+
+
+k=Solution()
+print(k.canFinish(4,[[1,0],[2,1],[0,2],[0,3]]))
