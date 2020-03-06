6ec7e4189e2b5fdaaba12fe1af7b5aa33908ecbd
jieD <38785315+huangketsudou@users.noreply.github.com>
2020-03-06 22:41:25 +0800

    leetcode-260

---
+++ digit/singleNumber.py
@@ -0,0 +1,21 @@
+class Solution:
+    def singleNumber(self, nums: List) -> List[int]:
+        # difference between two numbers (x and y) which were seen only once
+        bitmask = 0
+        for num in nums:
+            bitmask ^= num
+
+        # rightmost 1-bit diff between x and y
+        diff = bitmask & (-bitmask)
+        #bitmask最右边的1要么来自x，要么来自y，x & （-x）的操作可以保留最右边的1
+        #通过diff标记可以找到其中一个，另一个就是bitmask ^ x
+
+        x = 0
+        for num in nums:
+            # bitmask which will contain only x
+            if num & diff:
+                x ^= num
+
+        return [x, bitmask ^ x]
+
+
