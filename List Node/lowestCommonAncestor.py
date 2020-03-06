b8cd12f2ea1f04280c1abc90fccbb9f90d2cb0c8
jieD <38785315+huangketsudou@users.noreply.github.com>
2020-03-06 13:11:34 +0800

    Update lowestCommonAncestor.py

--- List Node/lowestCommonAncestor.py
+++ List Node/lowestCommonAncestor.py
@@ -3,7 +3,7 @@ class TreeNode:
         self.val = x
         self.left = None
         self.right = None
-
+#二叉搜索树
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         node = root
@@ -14,3 +14,154 @@ class Solution:
                 node = node.right
             else:
                 return node
+
+#------------------------普通·二叉树           
+            
+from typing import List
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    #普通二叉树
+    def __init__(self):
+        self.ans=None
+
+
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        def recursive(root):
+            if not root:return False
+
+            left=recursive(root.left)
+            right=recursive(root.right)
+            mid = False
+            if root==p or root==q:
+                mid=True
+
+            if mid + left+right>=2:
+                self.ans = root
+
+            return mid or left or right
+
+        recursive(root)
+
+        return self.ans
+
+
+class Solution2:
+
+    def lowestCommonAncestor(self, root, p, q):
+
+        stack=[root]
+        parent={root:None}
+
+        while p not in parent or q not in parent:
+            node=stack.pop()
+            if node.left:
+                stack.append(node.left)
+                parent[node.left]=node
+            if node.right:
+                stack.append(node.right)
+                parent[node.right]=node
+        ancestor=p
+        path=[]
+        while ancestor:
+            path.append(ancestor)
+            ancestor=path[ancestor]
+
+        while q not in path:
+            q=path[q]
+        return q
+
+
+
+class Solution:
+
+    # Three static flags to keep track of post-order traversal.
+
+    # Both left and right traversal pending for a node.
+    # Indicates the nodes children are yet to be traversed.
+    BOTH_PENDING = 2
+    # Left traversal done.
+    LEFT_DONE = 1
+    # Both left and right traversal done for a node.
+    # Indicates the node can be popped off the stack.
+    BOTH_DONE = 0
+
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+
+        # Initialize the stack with the root node.
+        stack = [(root, Solution.BOTH_PENDING)]
+
+        # This flag is set when either one of p or q is found.
+        one_node_found = False
+
+        # This is used to keep track of LCA index.
+        LCA_index = -1
+
+        # We do a post order traversal of the binary tree using stack
+        while stack:
+
+            parent_node, parent_state = stack[-1]
+
+            # If the parent_state is not equal to BOTH_DONE,
+            # this means the parent_node can't be popped of yet.
+            if parent_state != Solution.BOTH_DONE:
+
+                # If both child traversals are pending
+                if parent_state == Solution.BOTH_PENDING:
+
+                    # Check if the current parent_node is either p or q.
+                    if parent_node == p or parent_node == q:
+
+                        # If one_node_found is set already, this means we have found both the nodes.
+                        if one_node_found:
+                            return stack[LCA_index][0]
+                        else:
+                            # Otherwise, set one_node_found to True,
+                            # to mark one of p and q is found.
+                            one_node_found = True
+
+                            # Save the current top index of stack as the LCA_index.
+                            LCA_index = len(stack) - 1
+
+                    # If both pending, traverse the left child first
+                    child_node = parent_node.left
+                else:
+                    # traverse right child
+                    child_node = parent_node.right
+
+                # Update the node state at the top of the stack
+                # Since we have visited one more child.
+                stack.pop()
+                stack.append((parent_node, parent_state - 1))
+
+                # Add the child node to the stack for traversal.
+                if child_node:
+                    stack.append((child_node, Solution.BOTH_PENDING))
+            else:
+
+                # If the parent_state of the node is both done,
+                # the top node could be popped off the stack.
+
+                # i.e. If LCA_index is equal to length of stack. Then we decrease LCA_index by 1.
+                if one_node_found and LCA_index == len(stack) - 1:
+                    LCA_index -= 1
+                stack.pop()
+
+        return None
+
+
