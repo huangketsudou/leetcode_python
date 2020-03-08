941f535919f757dd2bc4d1c01acaf5ee4cdfb887
jieD <38785315+huangketsudou@users.noreply.github.com>
2020-03-09 00:29:07 +0800

    leetcode-141-142

--- List Node/hascycle.py
+++ List Node/hascycle.py
@@ -36,3 +36,43 @@ N5.next=N1
 
 k=Solution()
 print(k.hasCycle(N1))
+
+
+#https://leetcode-cn.com/problems/linked-list-cycle-ii/solution/huan-xing-lian-biao-ii-by-leetcode/
+#重要的分析过程
+class Solution(object):
+    def getIntersect(self, head):
+        tortoise = head
+        hare = head
+
+        # A fast pointer will either loop around a cycle and meet the slow
+        # pointer or reach the `null` at the end of a non-cyclic list.
+        while hare is not None and hare.next is not None:
+            tortoise = tortoise.next
+            hare = hare.next.next
+            if tortoise == hare:
+                return tortoise
+
+        return None
+
+    def detectCycle(self, head):
+        if head is None:
+            return None
+
+        # If there is a cycle, the fast/slow pointers will intersect at some
+        # node. Otherwise, there is no cycle, so we cannot find an e***ance to
+        # a cycle.
+        intersect = self.getIntersect(head)
+        if intersect is None:
+            return None
+
+        # To find the e***ance to the cycle, we have two pointers traverse at
+        # the same speed -- one from the front of the list, and the other from
+        # the point of intersection.
+        ptr1 = head
+        ptr2 = intersect
+        while ptr1 != ptr2:
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next
+
+        return ptr1
