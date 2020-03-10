a45ca0da9bef41d0225ef943c65476ff319237ac
jieD <38785315+huangketsudou@users.noreply.github.com>
2020-03-10 21:45:31 +0800

    leetcode-401

--- string/readBinaryWatch.py
+++ string/readBinaryWatch.py
@@ -3,5 +3,5 @@ class Solution:
         return [f"{h}:{m:02d}" for h in range(12) for m in range(60) if (bin(h)+bin(m)).count('1') == num]
     
     
-#找到小时与分钟的二进制1的位数为num就加入结果中
+#找到小时与分钟的二进制1的位数为num就加入结果中，从结果出发
 
