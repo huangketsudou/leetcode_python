efbbd0fa226c2154acb45144e2262c9dcd84311e
jieD <38785315+huangketsudou@users.noreply.github.com>
2020-03-10 21:44:47 +0800

    Update readBinaryWatch.py

--- string/readBinaryWatch.py
+++ string/readBinaryWatch.py
@@ -1,4 +1,7 @@
 class Solution:
     def readBinaryWatch(self, num: int) -> List[str]:
         return [f"{h}:{m:02d}" for h in range(12) for m in range(60) if (bin(h)+bin(m)).count('1') == num]
+    
+    
+#找到小时与分钟的二进制1的位数为num就加入结果中
 
