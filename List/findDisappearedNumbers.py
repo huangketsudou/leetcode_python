86344a12d2e857792a4916134261ab5e5563090e
jieD <38785315+huangketsudou@users.noreply.github.com>
2020-03-12 14:25:42 +0800

    leetcode-453

---
+++ List/findDisappearedNumbers.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        n=len(nums)
+        for i in range(n):
+            while nums[i]!=i+1:
+                if nums[nums[i]-1]==nums[i]:
+                    break
+                nums[nums[i]-1],nums[i]=nums[i],nums[nums[i]-1]
+        res=[]
+        for i in range(n):
+            if nums[i]!=i+1:
+                res.append(i+1)
+        return res
