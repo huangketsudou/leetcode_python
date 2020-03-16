e15765bbcf8cb745cc73cd974a0962a09826c77e
jieD <38785315+huangketsudou@users.noreply.github.com>
2020-03-16 11:50:26 +0800

    leetcode-5

--- string/LongestPalindrome.py
+++ string/LongestPalindrome.py
@@ -57,3 +57,29 @@ class Solution:
             left -= 1
             right += 1
         return right - left - 1
+
+    
+    
+class Solution:
+    #动态规划-转移方程dp[i][j]=dp[i+1][j-1] and s[i]==s[j]
+    def longestPalindrome(self, s: str) -> str:
+        if len(s)<2:return s
+        n=len(s)
+        dp=[[0]*n for _ in range(n)]
+        for i in range(n):
+            dp[i][i]=1
+        maxlen=1
+        start=0
+        for j in range(1,n):
+            for i in range(j):
+                if s[i]==s[j]:
+                    if j-i<3:
+                        dp[i][j]=1
+                    else:
+                        dp[i][j]=dp[i+1][j-1]
+                if dp[i][j]:
+                    curlen=j-i+1
+                    if curlen>maxlen:
+                        maxlen=curlen
+                        start=i
+        return s[start:start+maxlen]
