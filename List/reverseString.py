f55c4718a0d943e5e74bc5937eae07ee3fc00236
jieD <38785315+huangketsudou@users.noreply.github.com>
2020-03-16 14:48:51 +0800

    leetcode-344/541/557

--- List/reverseString.py
+++ List/reverseString.py
@@ -23,3 +23,20 @@ class Solution(object):
         for i in range(0, len(a), 2*k):
             a[i:i+k] = reversed(a[i:i+k])
         return "".join(a)
+class Solution:
+    def reverseWords(self, s: str) -> str:
+
+        l=s.split(' ')
+        rl=list(map(lambda x:x[::-1],l))
+        return ' '.join(rl)
+
+
+k=Solution()
+print(k.reverseWords("Let's take LeetCode contest"))
+
+
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return ' '.join(s.split(' ')[::-1])[::-1]
+
