1421411758382348a1fe3ab75d32354388e051a8
jieD <38785315+huangketsudou@users.noreply.github.com>
2020-03-27 13:54:28 +0800

    leetcode-690

---
+++ List Node/getImportance.py
@@ -0,0 +1,19 @@
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        # It's the unique id of each node.
+        # unique id of this employee
+        self.id = id
+        # the importance value of this employee
+        self.importance = importance
+        # the id of direct subordinates
+        self.subordinates = subordinates
+
+class Solution(object):
+    def getImportance(self, employees, query_id):
+        emap = {e.id: e for e in employees}
+        def dfs(eid):
+            employee = emap[eid]
+            return (employee.importance +
+                    sum(dfs(eid) for eid in employee.subordinates))
+        return dfs(query_id)
+
