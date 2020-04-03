60a70157ccd883cb2af1467675418a41b1725812
jieD <38785315+huangketsudou@users.noreply.github.com>
2020-04-03 12:23:43 +0800

    leetcode-819

---
+++ string/mostCommonWord.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def mostCommonWord(self, paragraph, banned):
+        banset = set(banned)
+        for c in "!?',;.":
+            paragraph = paragraph.replace(c, " ")
+        count = collections.Counter(
+            word for word in paragraph.lower().split())
+
+        ans, best = '', 0
+        for word in count:
+            if count[word] > best and word not in banset:
+                ans, best = word, count[word]
+
+        return ans
+
+
+k=Solution()
+print(k.mostCommonWord(paragraph = "Bob",banned = []))
