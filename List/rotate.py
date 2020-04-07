8d39eb2b4223e0ab5b748e4c9ccd0df7390e6672
jieD <38785315+huangketsudou@users.noreply.github.com>
2020-04-07 10:21:45 +0800

    leetcode-面试题01.07

--- List/rotate.py
+++ List/rotate.py
@@ -19,6 +19,46 @@ class Solution:
                     matrix[i][j]=matrix[n-1-j][i]
         del record
 
+        
+        
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        if n == 0: return
+        i = 0
+        while i < n // 2:
+            j = i
+            while j < n - i - 1:
+
+                cursorx, cursory = i, j
+                currentnumber = matrix[cursorx][cursory]
+                count = 0
+                while count < 4:
+                    print(cursorx, cursory)
+                    nx, ny = cursory, n - 1 - cursorx
+                    tmp = matrix[nx][ny]
+                    matrix[nx][ny] = currentnumber
+                    currentnumber = tmp
+                    cursorx, cursory = nx, ny
+                    count += 1
+                j += 1
+            i += 1
+
+
+class Solution2:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        # 水平翻转
+        for i in range(n // 2):
+            for j in range(n):
+                matrix[i][j], matrix[n - i - 1][j] = matrix[n - i - 1][j], matrix[i][j]
+        # 主对角线翻转
+        for i in range(n):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
 k=Solution()
 matrix =[
