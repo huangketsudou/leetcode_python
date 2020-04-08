814afc25feb078a132c20023d3814d6538ec17aa
jieD <38785315+huangketsudou@users.noreply.github.com>
2020-04-08 20:54:05 +0800

    leetcode-897

---
+++ List Node/increasingBST.py
@@ -0,0 +1,50 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        stack=deque()
+        node=root
+        newroot=record=TreeNode(-1)
+        while node or stack:
+            while node:
+                stack.append(node)
+                node=node.left
+            node=stack.pop()
+            newroot.right=node
+            newroot=newroot.right
+            newroot.left=None
+            node=node.right
+        return record.right
+
+class Solution:
+    def increasingBST(self, root):
+        def inorder(node):
+            if node:
+                inorder(node.left)
+                node.left = None
+                self.cur.right = node
+                self.cur = node
+                inorder(node.right)
+
+        ans = self.cur = TreeNode(None)
+        inorder(root)
+        return ans.right
+
+
+class Solution:
+    def increasingBST(self, root):
+        def inorder(node):
+            if node:
+                yield from inorder(node.left)
+                yield node.val
+                yield from inorder(node.right)
+
+        ans = cur = TreeNode(None)
+        for v in inorder(root):
+            cur.right = TreeNode(v)
+            cur = cur.right
+        return ans.right
