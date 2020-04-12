79bff970828fb2349c0097f0c8d4e5f84736df10
jieD <38785315+huangketsudou@users.noreply.github.com>
2020-04-12 14:03:43 +0800

    leetcode-5382

--- string/entityParser.py
+++ string/entityParser.py
@@ -9,3 +9,22 @@ class Solution3:
 
 k = Solution3()
 print(k.entityParser(text="leetcode.com&frasl;problemset&frasl;all"))
+
+
+class Solution:
+    def entityParser(self, text: str) -> str:
+        #题目里是有起始字符的&和终止字符；
+        dic = {'&quot;': '\"', '&apos;': '\'', '&amp;': '&', '&gt;': '>', '&lt;': '<', '&frasl;': '/'}
+        n, right = len(text), 0
+        curStr, res = "", ""
+        while right < n:
+            if right < n and text[right] != '&':
+                res += text[right]; right += 1
+            else:
+                curStr = ""
+                while right < n and text[right] != ';':
+                    curStr += text[right]; right += 1
+                curStr += ";"; right += 1
+                if curStr in dic: res += dic[curStr]
+                else: res += curStr
+        return res
