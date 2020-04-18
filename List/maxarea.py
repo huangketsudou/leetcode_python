4245904c01272c4025d6b112e63035b54b68d557
1941161938@qq.com <1941161938@qq.com>
2020-04-18 10:05:50 +0800

    leetcode-11

--- List/maxarea.py
+++ List/maxarea.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         head=0
@@ -10,4 +13,7 @@ class Solution:
             else:
                 maxarea = max((end - head) * height[end], maxarea)
                 end-=1
+            #在这里，对于height[start]==height[end]这种情况不需要讨论。因为题目只关心最大值
+            #而随着end，start之间的差距变小，中间的盛水要比两边的大，就必然有中间有两个高度大于两边
+            #按上面的策略缩放两边，最终会被取到，而如果没有，那形成的面积必定小于此时的end，start形成的面积
         return maxarea
