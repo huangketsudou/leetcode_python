a080ae83f98958d9bfea261722faeafd0fd7a527
1941161938@qq.com <1941161938@qq.com>
2020-04-25 13:45:58 +0800

    leetcode-347

---
+++ List/topKFrequent.py
@@ -0,0 +1,13 @@
+import collections,heapq
+
+
+class Solution:
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        count = collections.Counter(nums)
+        return heapq.nlargest(k, count.keys(), key=count.get)
+
