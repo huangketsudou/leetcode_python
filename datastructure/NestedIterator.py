6e470d68d4d2a7b5cc873dd292f22181887fa7eb
1941161938@qq.com <1941161938@qq.com>
2020-04-25 11:44:13 +0800

    leetcode-341

---
+++ datastructure/NestedIterator.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+class NestedInteger:
+    def isInteger(self) -> bool:
+        """
+        @return True if this NestedInteger holds a single integer, rather than a nested list.
+        """
+
+    def getInteger(self) -> int:
+        """
+        @return the single integer that this NestedInteger holds, if it holds a single integer
+        Return None if this NestedInteger holds a nested list
+        """
+
+    def getList(self) -> [NestedInteger]:
+        """
+        @return the nested list that this NestedInteger holds, if it holds a nested list
+        Return None if this NestedInteger holds a single integer
+ #        """
+
+
+class NestedIterator:
+    def __init__(self, nestedList: [NestedInteger]):
+        # 对于nestedList中的内容，我们需要从左往右遍历，
+        # 但堆栈pop是从右端开始，所以我们压栈的时候需要将nestedList反转再压栈
+        self.stack = nestedList[::-1]
+
+    def next(self) -> int:
+        # hasNext 函数中已经保证栈顶是integer，所以直接返回pop结果
+        return self.stack.pop(-1).getInteger()
+
+    def hasNext(self) -> bool:
+        # 对栈顶进行‘剥皮’，如果栈顶是List，把List反转再依次压栈，
+        # 然后再看栈顶，依次循环直到栈顶为Integer。
+        # 同时可以处理空的List，类似[[[]],[]]这种test case
+        while len(self.stack) > 0 and self.stack[-1].isInteger() is False:
+            self.stack += self.stack.pop().getList()[::-1]
+        return len(self.stack) > 0
+
+
