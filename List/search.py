5006b449ec1e20c89a42c078b54df867d5661b02
1941161938@qq.com <1941161938@qq.com>
2020-04-27 10:21:57 +0800

    leetcode-33/81

--- List/search.py
+++ List/search.py
@@ -25,6 +25,28 @@ class Solution:
 
         return -1
 
+class Solution:
+    #常规做法，
+    def search(self, nums: List[int], target: int) -> int:
+        n=len(nums)
+        left,right=0,n-1
+        while left<=right:
+            mid=(left+right)//2
+            if nums[mid]==target:
+                return mid
+            if nums[mid]>=nums[left]:#注意这里的等号是需要的
+                if nums[left]<=target<nums[mid]:
+                    right=mid-1
+                else:
+                    left=mid+1
+            else:
+                if nums[mid]<target<=nums[right]:
+                    left=mid+1
+                else:
+                    right=mid-1
+        return -1
+
+
 k=Solution()
 print(k.search([5,5,6,1,2,3],4))
 
