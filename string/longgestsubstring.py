91cd910b283d43277e9ec25c51ed9d00d2fd6bfb
1941161938@qq.com <1941161938@qq.com>
2020-05-02 15:12:58 +0800

    leetcode-3

--- string/longgestsubstring.py
+++ string/longgestsubstring.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         length=len(s)
@@ -8,10 +10,26 @@ class Solution:
         k=0
         for i in range(length):
             if  s[i] in fail:
-                k=max(fail[s[i]],k)
+                k=max(fail[s[i]],k)#k决定左边是哪里，当有fail[s[i]]<k时，就证明这些字母已经被删掉了
             MAX=max(MAX,i-k+1)
             fail[s[i]]=i+1
         return MAX
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        meet=set()
+        stack=deque()
+        ans=0
+        for c in s:
+            if c in meet:
+                while c in meet:
+                    top=stack.popleft()
+                    meet.remove(top)
+            meet.add(c)
+            stack.append(c)
+            ans=max(ans,len(stack))
+        return ans
+
 
 c=Solution()
 print(c.lengthOfLongestSubstring('abba'))
