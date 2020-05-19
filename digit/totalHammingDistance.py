c78d1956779674bc60166815512a5d36f451b7d0
1941161938@qq.com <1941161938@qq.com>
2020-05-19 15:00:26 +0800

    leetcode-477

---
+++ digit/totalHammingDistance.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class Solution:
+    def totalHammingDistance(self, nums: List[int]) -> int:
+        return sum((b.count('0') * b.count('1')) for b in zip(*map('{:032b}'.format, nums)))
