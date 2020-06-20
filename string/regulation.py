dc1aca1244aa45e093bee5aff4a60655ca67323c
1941161938@qq.com <1941161938@qq.com>
2020-06-20 10:54:45 +0800

    leetcode-10

--- string/regulation.py
+++ string/regulation.py
@@ -1,24 +1,21 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        if s==p:
+        if s == p:
             return True
         if not p and s:
             return False
-        if len(p)>1 and p[1]=='*':
-            if len(s)!=0 and(p[0]==s[0] or p[0]=='.'):
-                return self.isMatch(s[1:],p)  or self.isMatch(s,p[2:])
+        if len(p) > 1 and p[1] == '*':
+            if len(s) != 0 and (p[0] == s[0] or p[0] == '.'):
+                return self.isMatch(s[1:], p) or self.isMatch(s, p[2:])
             else:
-                return self.isMatch(s,p[2:])
+                return self.isMatch(s, p[2:])
         else:
-            if s and (p[0]==s[0] or p[0]=='.'):
-                return self.isMatch(s[1:],p[1:])
+            if s and (p[0] == s[0] or p[0] == '.'):
+                return self.isMatch(s[1:], p[1:])
         return False
-        #time limit exceed
-        
-        
-        
-        
-        
+        # time limit exceed
+
+
 class Solution2(object):
     def isMatch(self, text, pattern):
         if not pattern:
@@ -36,15 +33,15 @@ class Solution2(object):
 class Solution3(object):
     def isMatch(self, text, pattern):
         dp = [[False] * (len(pattern) + 1) for _ in range(len(text) + 1)]
-        dp[-1][-1] = True#标记均为两序列末尾这种情况为TRUE
+        dp[-1][-1] = True  # 标记均为两序列末尾这种情况为TRUE
         for i in range(len(text), -1, -1):
             for j in range(len(pattern) - 1, -1, -1):
-                #搜索是否pattern里有符合s*这种模式的，末尾可以匹配这种情况
+                # 搜索是否pattern里有符合s*这种模式的，末尾可以匹配这种情况
                 first_match = i < len(text) and pattern[j] in {text[i], '.'}
-                if j+1 < len(pattern) and pattern[j+1] == '*':
-                    dp[i][j] = dp[i][j+2] or first_match and dp[i+1][j]
+                if j + 1 < len(pattern) and pattern[j + 1] == '*':
+                    dp[i][j] = dp[i][j + 2] or first_match and dp[i + 1][j]
                 else:
-                    dp[i][j] = first_match and dp[i+1][j+1]
+                    dp[i][j] = first_match and dp[i + 1][j + 1]
         print(dp)
         return dp[0][0]
 
@@ -52,18 +49,38 @@ class Solution3(object):
 class Solution4(object):
     def isMatch(self, text, pattern):
         memo = {}
+
         def dp(i, j):
             if (i, j) not in memo:
                 if j == len(pattern):
                     ans = i == len(text)
                 else:
                     first_match = i < len(text) and pattern[j] in {text[i], '.'}
-                    if j+1 < len(pattern) and pattern[j+1] == '*':
-                        ans = dp(i, j+2) or first_match and dp(i+1, j)
+                    if j + 1 < len(pattern) and pattern[j + 1] == '*':
+                        ans = dp(i, j + 2) or first_match and dp(i + 1, j)
                     else:
-                        ans = first_match and dp(i+1, j+1)
+                        ans = first_match and dp(i + 1, j + 1)
 
                 memo[i, j] = ans
             return memo[i, j]
 
         return dp(0, 0)
+
+
+class Solution5:
+    def isMatch(self, text, pattern):
+        m, n = len(text), len(pattern)
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        for i in range(m + 1):
+            if i == 0:
+                dp[i][0] = True
+            elif pattern[i - 1] == '*':
+                dp[i][0] = dp[i - 2][0]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if pattern[i - 1] != "*":
+                    dp[i][j] = dp[i - 1][j - 1] and (pattern[i - 1] == '.' or pattern[i - 1] == text[j - 1])
+                else:
+                    dp[i][j] = dp[i - 2][j] or (
+                                (pattern[i - 2] == '.' or pattern[i - 2] == text[j - 1]) and dp[i][j - 1])
+        return dp[m][n]
